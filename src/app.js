5979f6572b76f6f501be167b5e44824e74ce4133
Raihan Revi Kasya <raihanrevikasya12@gmail.com>
2023-12-05 14:49:11 +0700

    update API include number in message

--- src/app.js
+++ src/app.js
@@ -6,21 +6,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/notificationsBanned', async (req, res) => {
+
+app.post('/notificationBanned', async (req, res) => {
+  const { number, message } = req.body;
   try {
-    const result = await sendBannedNotification(req.body.message);
-    res.status(201).json(result);
+    await sendBannedNotification(number, message);
+    res.status(200).json({ success: true, message: 'Notification sent successfully.' });
   } catch (error) {
-    res.status(500).json(error);
+    console.error('Error sending expired notification:', error.message);
+    res.status(500).json({ success: false, message: 'Failed to send notification.' });
   }
 });
 
-app.post('/notificationsExpired', async (req, res) => {
+app.post('/notificationExpired', async (req, res) => {
+  const { number, message } = req.body;
   try {
-    const result = await sendExpiredNotification(req.body.message);
-    res.status(201).json(result);
+    await sendExpiredNotification(number, message);
+    res.status(200).json({ success: true, message: 'Notification sent successfully.' });
   } catch (error) {
-    res.status(500).json(error);
+    console.error('Error sending expired notification:', error.message);
+    res.status(500).json({ success: false, message: 'Failed to send notification.' });
   }
 });
 
