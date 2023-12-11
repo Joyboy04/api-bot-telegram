0e92cf0a5399bf5c449bb27a89f9e4fb8273c621
Raihan Revi Kasya <raihanrevikasya12@gmail.com>
2023-12-11 11:09:46 +0700

    make it just 1 parameters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { sendBannedNotification, sendExpiredNotification } from './routes/routesNotifcation.js';
+import { sendBannedNotification } from './routes/routesNotifcation.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,21 +7,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 
-app.post('/notificationBanned', async (req, res) => {
+app.post('/notificationTele', async (req, res) => {
   const { number, message } = req.body;
   try {
     await sendBannedNotification(number, message);
-    res.status(200).json({ success: true, message: 'Notification sent successfully.' });
-  } catch (error) {
-    console.error('Error sending expired notification:', error.message);
-    res.status(500).json({ success: false, message: 'Failed to send notification.' });
-  }
-});
-
-app.post('/notificationExpired', async (req, res) => {
-  const { number, message } = req.body;
-  try {
-    await sendExpiredNotification(number, message);
     res.status(200).json({ success: true, message: 'Notification sent successfully.' });
   } catch (error) {
     console.error('Error sending expired notification:', error.message);
--- src/routes/routesNotifcation.js
+++ src/routes/routesNotifcation.js
@@ -13,17 +13,3 @@ export async function sendBannedNotification(number, message = 'Your Whatsapp Go
     throw { success: false, message: 'Failed to send notification.' };
   }
 }
-
-export async function sendExpiredNotification(number, message = 'Your Whatsapp Got Expired!') {
-  try {
-    const timestamp = new Date();
-    const date = new Intl.DateTimeFormat('id-ID', { dateStyle: 'full', timeZone: 'Asia/Jakarta' }).format(timestamp);
-    const time = new Intl.DateTimeFormat('id-ID', { timeStyle: 'long', timeZone: 'Asia/Jakarta' }).format(timestamp);
-    const fullMessage = `${message}\n\nNumber: ${number}\n\n${date}\n${time}`;
-    await sendTelegramMessage(fullMessage);
-    return { success: true, message: 'Notification sent successfully.' };
-  } catch (error) {
-    console.error('Error sending expired notification:', error.message);
-    throw { success: false, message: 'Failed to send notification.' };
-  }
-}
